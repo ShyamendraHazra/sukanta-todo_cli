55a4219fa9d5a5b75cc8230ea7ce9607c3d8d65a
Shyamendra Hazra <shyamendrahazra@gmail.com>
2025-11-02 19:21:31 +0530

    Automated Installation added!

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "Hello From the todocli!"
+TARGET_DIR="$HOME/.todo_cli"
+
+git clone https://github.com/su1nta/todo_cli.git $TARGET_DIR
+cd todo_cli
+npm install
+chmod +x todo.js
+npm link
+
+echo "Todo-cli is now installed! You may need to restart your shell to start using it."
